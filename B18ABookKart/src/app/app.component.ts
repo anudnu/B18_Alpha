943a0c5623cb5572da818d426c724ed1640539eb
anudnu <mail2anushapriya.s@gmail.com>
2022-03-13 13:12:20 +0530

    Committed

--- B18ABookKart/src/app/app.component.ts
+++ B18ABookKart/src/app/app.component.ts
@@ -11,37 +11,4 @@ import { ApiService } from './services/api.service';
 })
 export class AppComponent {
   title = 'B17com';
-//   category: string ="";
-//   public books!: Book[];
-//   public filteredProducts!: Book[];
-//  constructor(private data : ApiService,private route: ActivatedRoute){
-
-//  }
-
-//   getBooks() {
-//     this.data.books$.pipe(switchMap(
-//       (data: Book[]) => {
-//         this.filteredProducts = data;
-//         return this.route.queryParams;
-//       }
-//     )).subscribe((params:any) => {
-//       this.category = params.category;
-     
-//     });
-//   }
-
-//   filterBookData() {
-//     const filteredData = this.filteredProducts.filter(b => b.price <= this.priceRange).slice();
-
-//     if (this.category) {
-//       this.books = filteredData.filter(b => b.category.toLowerCase() === this.category.toLowerCase());
-//     } else if (this.searchItem) {
-//       this.books = filteredData.filter(b => b.title.toLowerCase().indexOf(this.searchItem) !== -1
-//         || b.author.toLowerCase().indexOf(this.searchItem) !== -1);
-//     } else {
-//       this.books = filteredData;
-//     }
-//     this.isLoading = false;
-//   }
-
 }
